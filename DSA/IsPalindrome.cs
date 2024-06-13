c37f7d167257bd720ce0b8cd4b26238f69fba570
Home <Home@DESKTOP-UMUT5UB>
2024-06-13 18:01:18 +0300

    Add project files.

---
+++ DSA/IsPalindrome.cs
@@ -0,0 +1,23 @@
+﻿// IsPalindrome For example, 121 is a palindrome while 123 is not.
+
+int x = 23;
+int rem, sum = 0, temp;
+temp = x;
+while (x > 0)
+{
+    rem = x % 10;
+    // Console.WriteLine(rem);
+    x = x / 10;
+   // Console.WriteLine(x);
+    sum = sum * 10 + rem;
+   //  Console.WriteLine(sum);
+
+}
+if (temp == sum)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}
